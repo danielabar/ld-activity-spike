743125dd2185fa708c6919539a823fff2233a914
Daniela Baron <baron.daniela@gmail.com>
2013-08-21 19:53:16 -0400

    introduce nested structure

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,5 @@
-// angular.module('ld-activity')
-// .controller('ActivityViewCtrl',
-// ['$scope', '$http', function($scope, $http) {
-
-// 	$http.get('data/activity.json').success(function(data) {
-//     	$scope.activities = data;
-//   	});
-
-// }]);
+// sample course outline: https://scs.senecac.on.ca/course/hwd101
+// json linter: http://jsonlint.com/
 
 function ActivityViewCtrl($scope, $http) {
 	$http.get('data/activity.json').success(function(data) {
