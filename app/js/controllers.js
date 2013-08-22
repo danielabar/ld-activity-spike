5c20b1e60abe1c5acd1f3b869cd306c120faa46b
Daniela Baron <baron.daniela@gmail.com>
2013-08-22 19:57:03 -0400

    add function for conditional styling

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,15 @@
 function ActivityViewCtrl($scope, $http) {
 	$http.get('data/activity2.json').success(function(data) {
     	$scope.levels = data;
-  	});  
+  	}); 
+
+  	$scope.navClass = function (node) {
+  		if (node.children) {
+  			return "groupBox4";
+  		}
+  		return "actBox";
+    };  
+
 }
 
 function ActivityEditCtrl($scope, $http) {
